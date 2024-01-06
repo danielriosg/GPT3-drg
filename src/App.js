07b3f534fdaa2866701aed947305bfb15ff501b7
Daniel Rios <ever.rios.gonzalez@gmail.com>
2024-01-06 13:32:41 -0500

    Add initial App.css and index.css

--- src/App.js
+++ src/App.js
@@ -8,9 +8,8 @@ import {
   WhatGPT3,
   Header,
 } from "./containers";
-
 import { CTA, Brand, Navbar } from "./components";
-
+import "./App.css";
 const App = () => {
   return (
     <div className='App'>
