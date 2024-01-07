4a59ac4e90f7100acaf745625fa7d45a85d11ce3
Daniel Rios <ever.rios.gonzalez@gmail.com>
2024-01-07 13:12:12 -0500

    Update mobile response

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,19 +1,11 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { RiMenu3Line, RiCloseLin, RiCloseLine } from "react-icons/ri";
 import logo from "../../assets/logo.svg";
 import "./navbar.css";
 
-const Navbar = () => {
-const [toggleMenu, setToggleMenu] = useState(false);
-
-  return (
-    <div className='gpt3__navbar'>
-      <div className='gpt3__navbar-links'>
-        <div className='gpt3__navbar-links_logo'>
-          <img src={logo} alt='logo' />
-        </div>
-        <div className='gpt3__navbar-links_container'>
-          <p>
+const Menu = () => (
+  <>
+  <p>
             <a href='#home'>Home</a>
           </p>
           <p>
@@ -28,6 +20,20 @@ const [toggleMenu, setToggleMenu] = useState(false);
           <p>
             <a href='#blog'>Library</a>
           </p>
+          </>
+)
+
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  return (
+    <div className='gpt3__navbar'>
+      <div className='gpt3__navbar-links'>
+        <div className='gpt3__navbar-links_logo'>
+          <img src={logo} alt='logo' />
+        </div>
+        <div className='gpt3__navbar-links_container'>
+          <Menu />
         </div>
       </div>
       <div className='gpt3__navbar-sign'>
@@ -47,6 +53,12 @@ const [toggleMenu, setToggleMenu] = useState(false);
             size={27}
             onClick={() => setToggleMenu(true)}
           />
+        )}
+        {toggleMenu && (
+          <div className='gpt3__navbar-,enu-container scale-up-center'>
+            <div className='gpt3__navbar-menu_container-links'></div>
+            <Menu />
+          </div>
         )}
       </div>
     </div>
